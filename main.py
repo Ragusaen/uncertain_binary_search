898808e53e387f9bc7e1b5acf51a8df109fa3c9d
Rasmus Tollund <rasmusgtollund@gmail.com>
2024-05-23 19:57:43 +0200

    Sped up the inference by chaching values

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
-from probabilistic_bisection2 import ProbabilisticBisection2
+import random
+import time
+
 from probability_table import entropy
 from test_simulator import TestSimulator
 from probabilistic_bisection import ProbabilisticBisection
 import cProfile
 import pstats
 
-N = 6
-c = 0.95
 
-sim = TestSimulator(N, 1.0, 0.1, seed=0)
-bis = ProbabilisticBisection2(N, discretisation_steps=10)
-print('Actual:', sim.true_positive_rate, sim.true_negative_rate, sim.bad_point)
-
-def run():
+def run(sim, bis, c = 0.95):
     steps = 0
+    print('Bad point', sim.bad_point)
     while True:
         steps += 1
         t = bis.best_test_point()
         r = sim.run_test(t)
-        print('entropy before', bis.entropy())
+        # print('entropy before', bis.entropy())
         bis.report_test_result(t, r == 'Pass')
-        print('entropy after', bis.entropy())
+        # print('entropy after', bis.entropy())
 
-        print(t, r, [bis.prob_point_bad(i) for i in range(N)])
-        print('\t', bis.theta_given_evidence())
-        if any(bis.prob_point_bad(i) >= c for i in range(N)):
-            assert max(range(N), key=bis.prob_point_bad) == sim.bad_point
-            break
+        print(t, r, [bis.prob_point_bad(i) for i in range(bis.num_points)])
+        # print('\t', bis.theta_given_evidence())
+        if any(bis.prob_point_bad(i) >= c for i in range(bis.num_points)):
+            return max(range(bis.num_points), key=bis.prob_point_bad)
 
-    print('Total tests: ', steps)
+def random_test():
+    for i in range(10000):
+        N = random.randint(3, 20)
+        sim = TestSimulator(N, 1.0, random.random() * 0.99 + 0.01)
+        bis = ProbabilisticBisection2(N)
+        print(N, sim.true_negative_rate)
 
-run()
+        if sim.bad_point == run(sim, bis):
+            print('right')
+        else:
+            print('wrong')
+
+t = time.time()
+run(TestSimulator(50, 1.0, 0.05, seed=0), ProbabilisticBisection(20))
+print(time.time() - t)
--- probabilistic_bisection.py
+++ probabilistic_bisection.py
@@ -1,17 +1,16 @@
+from pgmpy.factors.discrete import TabularCPD, DiscreteFactor
+from pgmpy.inference import VariableElimination, BeliefPropagation, DBNInference, ApproxInference
+from pgmpy.inference.EliminationOrder import BaseEliminationOrder
+from pgmpy.models import BayesianNetwork
 import numpy as np
 
-from probability_table import Variable, ProbabilityTable, entropy
 
+TabularCPD._truncate_strtable = lambda self, x: x
 
-# GOOD = 0
-# BAD = 1
-# PASS = 10
-# FAIL = 11
 GOOD = 'Good'
 BAD = 'Bad'
 PASS = 'Pass'
 FAIL = 'Fail'
-
 
 def point_var_idx(i: int):
     # return i
@@ -29,142 +28,140 @@ def theta_var_idx():
     # return -2
     return 'θ'
 
+
+def discrete_factor_log2(self):
+    with np.errstate(divide='ignore'):
+        self.values = np.nan_to_num(np.log2(self.values))
+
+DiscreteFactor.log2 = discrete_factor_log2
+
+
+def entropy(p: DiscreteFactor, q: DiscreteFactor = DiscreteFactor([], [], [1.]), with_vars: set = None):
+    """
+    Computes H(X|Y) from P(X,Y) and P(Y)
+    """
+    p_g = p / q
+    p_g.log2()
+    j = p * p_g
+    j.marginalize(set(p.variables) - (with_vars if with_vars else set()))
+    return -j.values
+
+
 class ProbabilisticBisection:
-    def __init__(self, num_points: int, p_bad: float = 0.9, good_certainty: float = 0.9, discretisation_steps: int = 6):
-        """
+    def __init__(self, num_points: int, discretisation_steps: int = 20):
+        self.theta_discretisation = [(j + 1) / discretisation_steps for j in range(discretisation_steps)]
+        self.num_points = num_points
+        self.tests = {i: {PASS: 0, FAIL: 0} for i in range(num_points)}
 
-        :param num_points: number of points to test
-        :param p_bad: probability that there actually is a reproducible error
-        """
-        self.good_offset = (num_points * 1 / (1 - good_certainty)) if good_certainty < 1 else None
+        self.G = BayesianNetwork()
 
-        self.error_var = Variable(error_var_idx(), list(range(num_points)))
-        self.error_pt = ProbabilityTable.from_dict(
-                    [self.error_var],
-                    {i: p_bad * (1 / num_points) if i < num_points else (1 - p_bad) for i in range(num_points + 1)},
-                    []
+        self.G.add_node(error_var_idx())
+        self.G.add_cpds(
+            TabularCPD(error_var_idx(),
+               num_points,
+               [[1 / num_points] for _ in range(num_points)],
+               state_names={error_var_idx(): [j for j in range(num_points)]}
+               )
         )
 
-        self.points = [Variable(point_var_idx(i), [GOOD, BAD]) for i in range(num_points)]
-        self.points_pts = [
-            ProbabilityTable.from_dict([self.error_var, self.points[i]], {
-                j: ({GOOD: 1, BAD: 0} if i < j or j == num_points else {GOOD: 0, BAD: 1}) for j in range(num_points + 1)
-            }, [self.error_var]) for i in range(num_points)
-        ]
+        self.G.add_node(theta_var_idx())
+        self.G.add_cpds(
+            TabularCPD(theta_var_idx(),
+                   discretisation_steps,
+                   [[1 / discretisation_steps] for _ in range(discretisation_steps)],
+                   state_names={theta_var_idx(): self.theta_discretisation}
+               )
+        )
 
-        self.theta_var = Variable(theta_var_idx(), [np.exp(-1/discretisation_steps * i**2) for i in range(discretisation_steps)])
-        self.theta_pt = ProbabilityTable.from_dict([self.theta_var], {s: 1 / discretisation_steps for s in self.theta_var.domain})
+        for i in range(num_points):
+            self.G.add_node(point_var_idx(i))
+            self.G.add_edge(error_var_idx(), point_var_idx(i))
+            self.G.add_cpds(
+                TabularCPD(point_var_idx(i),
+                    2,
+                    [[int(o) if i < j else int(not o) for j in range(num_points)] for o in [True, False]],
+                    evidence=[error_var_idx()],
+                    evidence_card=[num_points],
+                    state_names={point_var_idx(i): [GOOD, BAD], error_var_idx(): [j for j in range(num_points)]}
+               )
+            )
 
-        self.tests = [{PASS: 0, FAIL: 0} for _ in range(num_points)]
+        self.G.check_model()
+
+        self.test_nodes = [0 for _ in range(num_points)]
+
+        self.inference = VariableElimination(self.G)
+
+        self.query_cache = {}
+
+    def query(self, variables: list):
+        tupvar = tuple(variables)
+        if tupvar in self.query_cache:
+            return self.query_cache[tupvar]
+        res = self.inference.query(
+            variables,
+            evidence=self.get_test_evidence(),
+            # elimination_order=self.get_elimination_order(set(self.G.nodes) - set(variables) - set(self.get_test_evidence().keys())),
+            show_progress=False
+        )
+        self.query_cache[tupvar] = res
+        return res
+
+    def add_test_node(self, point: int):
+        self.query_cache.clear()
+        test_index = self.test_nodes[point]
+        self.test_nodes[point] += 1
+        self.G.add_node(test_var_idx(point, test_index))
+        self.G.add_edge(point_var_idx(point), test_var_idx(point, test_index))
+        self.G.add_edge(theta_var_idx(), test_var_idx(point, test_index))
+        self.G.add_cpds(
+            TabularCPD(
+                test_var_idx(point, test_index),
+                2,
+                [[1 if good else 1 - s for s in self.theta_discretisation for good in [True, False]],  # PASS
+                 [0 if good else s for s in self.theta_discretisation for good in [True, False]]],  # FAIL
+                [theta_var_idx(), point_var_idx(point)],
+                [len(self.theta_discretisation), 2],
+                state_names={test_var_idx(point, test_index): [PASS, FAIL], point_var_idx(point): [GOOD, BAD],
+                             theta_var_idx(): self.theta_discretisation}
+            )
+        )
+        return test_var_idx(point, test_index)
+
+    def remove_test_node(self, point: int):
+        self.query_cache.clear()
+        self.test_nodes[point] -= 1
+        test_index = self.test_nodes[point]
+        self.G.remove_node(test_var_idx(point, test_index))
 
     def report_test_result(self, point: int, passed: bool):
+        self.add_test_node(point)
         self.tests[point][PASS if passed else FAIL] += 1
-        if hasattr(self, 'prob_point_cache'):
-            delattr(self, 'prob_point_cache')
-        if hasattr(self, 'joint_probability_with_test_evidence_cache'):
-            delattr(self, 'joint_probability_with_test_evidence_cache')
 
-    def _test_probability_table(self, point: int, test: Variable):
-        return ProbabilityTable.from_dict([self.theta_var, self.points[point], test], {
-            s: {
-                GOOD: {
-                    PASS: 1,
-                    FAIL: 0,
-                },
-                BAD: {
-                    PASS: 1 - s,
-                    FAIL: s
-                }
-            } for s in self.theta_var.domain
-        }, [self.theta_var, self.points[point]])
-
-    def joint_probability(self):
-        """P(P_0,P_1,...,P_n) = P(P_0) * P(P_1|P_0) * ... * P(P_n|P_{n-1})"""
-        if hasattr(self, 'joint_probability_cache'):
-            return getattr(self, 'joint_probability_cache')
-
-        p = ProbabilityTable.unit()
-        for i in range(len(self.points)):
-            p *= self.points_pts[i]
-        self.joint_probability_cache = p
-        assert False
-        return p
-
-    def joint_probability_with_test_evidence(self, extra_test_point: int = -1):
-        """P(P_0,...,P_n|T_0=,T_1=,...)"""
-        p = self.error_pt
-        for i in range(len(self.points)):
-            p *= self.points_pts[i]
-            for j, result in enumerate([PASS] * self.tests[i][PASS] + [FAIL] * self.tests[i][FAIL]):
-                test_var = Variable(test_var_idx(i, j), [PASS, FAIL])
-                p *= self._test_probability_table(i, test_var)
-                q = p.marginalize_but(test_var, self.theta_var)
-                p = (p / q).evidence(test_var, result)
-
-            if extra_test_point == i:
-                test_var = Variable(test_var_idx(i, self.tests[i][PASS] + self.tests[i][FAIL]), [PASS, FAIL])
-                p *= self._test_probability_table(i, test_var)
-            p = p.marginalize(self.points[i])
-
-        p = (p * self.theta_pt).marginalize(self.theta_var)
-        return p
-
-    def information_gain(self, point: int):
-        # Return the information gain of running another test on this point
-        p_E_Ti = self.joint_probability_with_test_evidence(point)
-
-        return entropy(p_E_Ti.marginalize_but(self.error_var)) - entropy(p_E_Ti, p_E_Ti.marginalize(self.error_var))
-
-    def theta_given_evidence(self):
-        p_theta = self.theta_pt
-        for i in range(len(self.points)):
-            for j, result in enumerate([PASS] * self.tests[i][PASS] + [FAIL] * self.tests[i][FAIL]):
-                p_E_Xi = self.error_pt * self.points_pts[i]
-                test_var = Variable(test_var_idx(i, j), [PASS, FAIL])
-                p_E_Xi_Tij_theta = p_E_Xi * self._test_probability_table(i, test_var) * p_theta
-                p_E_theta_given_Tij = p_E_Xi_Tij_theta.marginalize(self.points[i]) / p_E_Xi_Tij_theta.marginalize_but(test_var)
-                p_theta = p_E_theta_given_Tij.evidence(test_var, result).marginalize(self.error_var)
-
-        return p_theta
-
-    def best_test_point(self):
-        print('IG', [self.information_gain(i) for i in range(len(self.points))])
-        return max(range(len(self.points)), key=self.information_gain)
-
-    def prob_point(self, point: int):
-        if hasattr(self, 'prob_point_cache') and point in getattr(self, 'prob_point_cache'):
-            return getattr(self, 'prob_point_cache')[point]
-
-        p_E_theta_given_T = self.joint_probability_with_test_evidence()
-        p_E_Xi_given_T = p_E_theta_given_T * self.points_pts[point]
-        p_Xi_given_T = p_E_Xi_given_T.marginalize(self.error_var, self.theta_var)
-
-        if not hasattr(self, 'prob_point_cache'):
-            self.prob_point_cache = {}
-        self.prob_point_cache[point] = p_Xi_given_T
-        return p_Xi_given_T
+    def get_test_evidence(self):
+        return {test_var_idx(i, j): PASS if j < self.tests[i][PASS] else FAIL for i in range(self.num_points) for j in range(self.tests[i][PASS] + self.tests[i][FAIL])}
 
     def prob_point_bad(self, point: int):
-        p_E_given_T = self.joint_probability_with_test_evidence()
-        return p_E_given_T.index({self.error_var: point})
+        q = self.query([error_var_idx()])
+        return q.values[point]
 
-    def update_theta(self):
-        if self.good_offset is not None:
-            self.true_positive_rate = (self.good_offset + sum(self.prob_point(i).index({self.points[i]: GOOD}) * (self.tests[i][PASS] + 1) for i in range(len(self.points)))) \
-                                  / (self.good_offset + sum(self.prob_point(i).index({self.points[i]: GOOD}) * (self.tests[i][PASS] + self.tests[i][FAIL] + 2) for i in range(len(self.points))))
-        else:
-            self.true_positive_rate = 1.0
-        self.true_negative_rate = sum(self.prob_point(i).index({self.points[i]: BAD}) * (self.tests[i][FAIL] + 1) for i in range(len(self.points))) \
-                                  / sum(self.prob_point(i).index({self.points[i]: BAD}) * (self.tests[i][PASS] + self.tests[i][FAIL] + 2) for i in range(len(self.points)))
+    def entropy(self):
+        p = self.query([error_var_idx()])
+        e = entropy(p)
+        return e
 
-        if hasattr(self, 'prob_point_cache'):
-            delattr(self, 'prob_point_cache')
-        if hasattr(self, 'joint_probability_with_test_evidence_cache'):
-            delattr(self, 'joint_probability_with_test_evidence_cache')
-        if hasattr(self, 'joint_probability_cache'):
-            delattr(self, 'joint_probability_cache')
+    def information_gain(self, point: int, with_test_var: bool = False):
+        test_var = self.add_test_node(point)
+        p = self.query([error_var_idx(), test_var])
+        ig = (entropy(p.marginalize([test_var], inplace=False), with_vars={test_var} if with_test_var else set())
+              - entropy(p, p.marginalize([error_var_idx()], inplace=False), with_vars={test_var} if with_test_var else set()))
+        self.remove_test_node(point)
+        return ig
 
-if __name__ == '__main__':
-    b = ProbabilisticBisection(3, 0.9)
-    print(b.joint_probability())
+    def best_test_point(self):
+        # print('IG', [self.information_gain(i, True) for i in range(self.num_points)])
+        return max(range(self.num_points), key=self.information_gain)
 
+    def theta_given_evidence(self):
+        p = self.query([theta_var_idx()])
+        return p
